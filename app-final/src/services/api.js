faf67cb53537a1deaa19a5d5b7ba699435182b55
kauasduarte <kauaduarte@gmail.com>
2023-12-02 13:55:56 -0300

    Update api.js

--- app-final/src/services/api.js
+++ app-final/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.nookipedia.com/',
   headers: {
-    'X-API-KEY': '1f22b6af-4f60-44b8-89b0-d9b6a87bc862', 
+    'X-API-KEY': '#', 
     'Content-Type': 'application/json',
   },
 });
